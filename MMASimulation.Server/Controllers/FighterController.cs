d386cca986a173e24327d90ab8ec1066b5698f7a
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-11-17 14:23:42 -0300

    Criando FighterController

---
+++ MMASimulation.Server/Controllers/FighterController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MMASimulation.Server.Services.FighterService;
+using MMASimulation.Shared.Dtos.Fighters;
+using MMASimulation.Shared.Models.Utils;
+
+namespace MMASimulation.Server.Controllers
+{
+    [Route("api/fighters")]
+    [ApiController]
+    public class FighterController : ControllerBase
+    {
+
+        private readonly IFighterService _fighterService;
+
+        public FighterController(IFighterService fighterService)
+        {
+            _fighterService = fighterService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ServiceResponse<List<FighterDto>>>> GetAllFighters()
+        {
+
+            var result = await _fighterService.GetAllFighters();
+            return Ok(result);
+
+        }
+
+    }
+}
