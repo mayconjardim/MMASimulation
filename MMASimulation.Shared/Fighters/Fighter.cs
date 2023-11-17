66d156f8d4f1ccda5754e4e0c00a26e7e0be2418
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-11-16 22:30:32 -0300

    Criando classe Fighter

---
+++ MMASimulation.Shared/Fighters/Fighter.cs
@@ -0,0 +1,15 @@
+﻿namespace MMASimulation.Shared.Fighters
+{
+    public class Fighter
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public int Wins { get; set; }
+        public int Loss { get; set; }
+        public int Draw { get; set; }
+
+    }
+}
